566c950ba48af4e5d5d241099e754f06ebfbe5dc
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2017-07-02 14:29:49 +0200

    regexI18n

--- code/evildata/src/main/java/be/ge0ffrey/presentations/evildata/EvilDataApp.java
+++ code/evildata/src/main/java/be/ge0ffrey/presentations/evildata/EvilDataApp.java
@@ -16,18 +16,21 @@
 
 package be.ge0ffrey.presentations.evildata;
 
+import java.util.regex.Pattern;
+
 public class EvilDataApp {
 
     public static void main(String[] args) {
         doubleRounding();
         doubleAssociate();
         doubleIncrement();
+        regexI18n();
     }
 
     private static void doubleRounding() {
         System.out.println("Double rounding");
         System.out.println("================\n");
-        System.out.println("     0.01 + 0.09 == 0.10: " + ((0.01 + 0.09) == 0.10)); // false
+        System.out.println("    0.01 + 0.09 == 0.10: " + ((0.01 + 0.09) == 0.10)); // false
         System.out.printf("\n   %s\n + %s\n = %s\n", 0.01, 0.09, 0.01 + 0.09);
         System.out.printf("\n   %s\n + %s\n = %s\n", 0.01, 0.05, 0.01 + 0.05);
         System.out.println();
@@ -52,6 +55,32 @@ public class EvilDataApp {
         System.out.printf("\n   %18.1f\n + %18.1f\n = %18.1f\n", a, 1.0, b);
         double c = a + 3.0;
         System.out.printf("\n   %18.1f\n + %18.1f\n = %18.1f\n", a, 3.0, c);
+        System.out.println();
+    }
+
+    private static void regexI18n() {
+        String validName = "ValidName";
+        String invalidName = "Invalid/name";
+        String japaneseValidName = "有効名";
+        String japaneseInvalidName = "無効/名";
+
+        String badRegex = "^\\w+$";
+        String goodRegex = "(?U)^\\w+$";
+
+        System.out.println("I18n in regular expressions");
+        System.out.println("===========================\n");
+        System.out.println();
+        System.out.println("    Regular expression \"" + badRegex + "\"");
+        System.out.println("        \"" + validName + "\" (valid): " + validName.matches(badRegex));
+        System.out.println("        \"" + invalidName + "\" (invalid): " + invalidName.matches(badRegex));
+        System.out.println("        \"" + japaneseValidName + " (valid)\": " + japaneseValidName.matches(badRegex) + " <---------- false negative");
+        System.out.println("        \"" + japaneseInvalidName + "\" (invalid): " + japaneseInvalidName.matches(badRegex));
+        System.out.println();
+        System.out.println("    Regular expression \"" + goodRegex + "\"");
+        System.out.println("        \"" + validName + "\" (valid): " + validName.matches(goodRegex));
+        System.out.println("        \"" + invalidName + "\" (invalid): " + invalidName.matches(goodRegex));
+        System.out.println("        \"" + japaneseValidName + "\" (valid): " + japaneseValidName.matches(goodRegex));
+        System.out.println("        \"" + japaneseInvalidName + "\" (invalid): " + japaneseInvalidName.matches(goodRegex));
         System.out.println();
     }
 
